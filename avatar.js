08c6856d2e0629eabdcc1d5b666a1c9c9b5cd1fa
valenarangot <valentina.arango.tenorio@gmail.com>
2022-11-13 16:00:46 -0500

    ¨Valen-slider¨

---
+++ avatar.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', function(){
+    new Glider(document.querySelector('.carousel__lista'),{
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: '.dots',
+    arrows: {
+        prev: '.carousel__anterior',
+        next: '.carousel__siguiente'
+  }
+    } )
+});
